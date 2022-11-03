47c25aeca8647c61731ebd8b9b10d199dc2761cc
Bruna Ceppa <87783719+Bcpp16@users.noreply.github.com>
2022-11-03 08:39:40 -0300

    Diseño de main

--- pokedex-app/src/components/header/Header.jsx
+++ pokedex-app/src/components/header/Header.jsx
@@ -1,47 +1,43 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 
-function Header({showData, dataDefault, listPokemones}) {
+function Header({ showData, dataDefault, listPokemones }) {
+  const onChangeInput = (e) => {
+    let textInput = e.target.value;
 
-    const onChangeInput = (e) =>{
-        let textInput = e.target.value;
+    if (textInput == "") {
+      return dataDefault();
+    }
 
-        if (textInput == '') {
-            return dataDefault();
-        }
+    const filter = listPokemones.filter((list) => {
+      if (list.name.includes(textInput)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
 
-        const filter = listPokemones.filter((list) => {
-            if (list.name.includes(textInput)) {
-                return true; 
-            }else{
-                return false;
-            }
-    
-        });
-
-        showData(filter);
-    };
-
+    showData(filter);
+  };
 
   return (
-    <div>
-      <div className='logo'>
-        <img className='img-logo' src="/public/assets/Pokebola.png" />
-        <p className='txt-logo'>Pokedex</p>
-      </div>
-    <div>
-      <div className='container-search'>
-      <input 
-        className="search"
-        placeholder="Search bar"
-        onChange={onChangeInput}
-        />
-       
+    <div className="cont1">
+      <div className="cont-header">
+        <div className="cont-logo">
+        <img className="img-logo" src="/public/assets/Pokebola.png" />
+        <span className="txt-logo">Pokédex</span>
+        </div>
+        <div className="cont-search">
+         <input
+            className="search"
+            placeholder="Buscar"
+            onChange={onChangeInput}
+          />
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- pokedex-app/src/components/home/Main.jsx
+++ pokedex-app/src/components/home/Main.jsx
@@ -1,40 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import {Link} from 'react-router-dom';
-import './main.css'
+import { Link } from "react-router-dom";
+import "./main.css";
 
-function Main({listPokemones}) {
-
-
+function Main({ listPokemones }) {
   return (
     <>
+      <div className="container-card">
+        {listPokemones.length ? (
+          listPokemones.map((pokemon) => {
+            return (
+              <Link to={`pokemon/${pokemon.id}`} className="link">
+                <div className="st-card">
+                  <div className="id">{`#${pokemon.id}`}</div>
+                  <img src={pokemon.image} />
 
-    <div 
-    className="container-card">
-      {listPokemones.length ? (
-        listPokemones.map((pokemon) => {
-          return (
-            <div className="st-card" style={{backgroundColor: pokemon.color}}>
-            <div className='back'>
-
-              <div className='id'>{`#${pokemon.id}`}</div>
-  <Link to={`pokemon/${pokemon.id}`}>
-              <img style={{color: pokemon.color}} src={pokemon.image} />
-  </Link>
-            </div>
-              
-
-
-              <div className='nombre' >{pokemon.name}</div>
-            </div>
-          );
-        })
-      ) : (
-        <p className="txt-main">No se han encontrado Pokemones</p>
-      )}
-    </div>
+                  <div
+                    className="bg-color"
+                    style={{ backgroundColor: pokemon.color }}
+                  >
+                    <div className="nombre">{pokemon.name}</div>
+                  </div>
+                </div>
+              </Link>
+            );
+          })
+        ) : (
+          <p className="txt-main">No se han encontrado Pokemones</p>
+        )}
+      </div>
+      <div className="footer"></div>
     </>
-  )
+  );
 }
 
 export default Main;
