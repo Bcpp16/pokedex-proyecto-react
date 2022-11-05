3058c5c4946bb570f86895bd0d90142b891151b2
ArianOC <ivomariane1@gmail.com>
2022-11-05 09:58:33 -0300

    comentario en la progressbar para cambio de color

--- pokedex-app/src/components/modal/Modal.jsx
+++ pokedex-app/src/components/modal/Modal.jsx
@@ -78,6 +78,7 @@ export default function ModalStates(props) {
               id="DEF"
               className="progress"
               max="200"
+              // backgroundColor={`../${pokemon.image}`}
               style={{ backgroundColor: pokemon.color }}
               value={pokemon.baseStats.DEF}
             ></progress>
